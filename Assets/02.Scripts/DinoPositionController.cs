666735b4a6b76bad8cda8e6bf682f6f2f750bb02
성민서 <seongminseo1010@gmail.com>
2025-05-31 13:40:48 +0900

    2025/5/31
    오늘은 여기까지.

--- Assets/02.Scripts/DinoPositionController.cs
+++ Assets/02.Scripts/DinoPositionController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class DinoPositionController : MonoBehaviour
 {
+    public Transform raptors;   // Raptor들을 관리할 부모 오브제그의 Transform
+
+    public float radius = 1f;   // 원의 반지름
+    public float ratio = 1f;  // 배치 간격 비율 (작을수록 촘촘하게)
     void Start()
     {
         
@@ -11,12 +15,40 @@ public class DinoPositionController : MonoBehaviour
 
     void Update()
     {
-        transform.GetChild(0).localPosition = Vector3.zero;
-        for (int i=1; i<=transform.childCount/2; i++)
+        SetDinoPosition();
+    }
+
+    private void SetDinoPosition()
+    {
+        // 원형 배치 알고리즘
+        for(int i=0; i<raptors.childCount; i++)
         {
-            float x = 3 * i;
-            transform.GetChild(2*i-1).localPosition = Vector3.right * x;
-            transform.GetChild(2*i).localPosition = Vector3.left * x;
+            if(i < 9)   // 9번째 공룡까지만 화면에 송출
+            {
+                if (raptors.childCount <= 9)    // 공룡 개수가 9개 이하일 때만 화면에 배치
+                {
+                    // 360도 각도 계산을 위한 각도 증가 값 (몇개가 배치될지 계산 값) (배치될 오브젝트들의 각도 간격)
+                    float angleSetp = 360f / (raptors.childCount * ratio);
+
+                    // 각 오브젝트의 배치 각도 계산
+                    float angle = i * angleSetp;
+
+                    // 각도를 라디안으로 변환
+                    float angleRad = Mathf.Deg2Rad * angle;
+                    // "Mathf.Rad2Deg"는 반대로 라디안을 각도로 변환하는 것
+
+                    // X와 Z 좌표를 원형으로 계산
+                    float x = Mathf.Cos(angleRad) * radius;
+                    float z = Mathf.Sin(angleRad) * radius;
+
+                    // 새로운 위치로 자식 오브젝트를 위치시킴
+                    raptors.GetChild(i).localPosition = new Vector3(x, 0, z);
+                }
+            }
+            else  // 10번째 공룡부터 화면에 송출 안함
+            {
+                raptors.GetChild(i).gameObject.SetActive(false);
+            }
         }
     }
 }
